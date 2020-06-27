91bbed1dc1a5e0068bad6e1b1f8b85aea175e752
prasanna7 <prasanna@livbim.io>
2020-06-27 13:32:29 +0530

    burger builder ui WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div >
-       <h1>Hello</h1>
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
     </div>
   );
 }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxi'
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <Burger/>
+                <div>burger</div>
+                <div>Burger Controls</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
